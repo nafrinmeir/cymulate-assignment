9d20914333ad9b33b8b4672e4cca25b3178fbadc
nafrinmeir <45077386+nafrinmeir@users.noreply.github.com>
2024-11-27 12:21:28 +0200

    Add files via upload

---
+++ docker-test.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+const app = new express();
+const port = 8080;
+
+
+app.get('/', function(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('Hello from cymulate!');
+});
+
+app.get('/assignment', function(request, response){
+    response.sendFile('cymulate-assignment-index.html', { root: __dirname });
+});     
+
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+    process.exit();
+});
+
+app.listen(port, () => {
+    console.log(`Starting Server at http://localhost:${port}`)
+})
